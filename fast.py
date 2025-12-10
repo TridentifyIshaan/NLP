9ef2fe033ad8d6159aa8437c32d8179664934e60
Ishaan Rastogi <97788132+TridentifyIshaan@users.noreply.github.com>
2025-12-10 13:56:29 +0000

    Fast API

--- fast.py
+++ fast.py
@@ -5,13 +5,28 @@ appn = FastAPI()
 
 @appn.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "World", "Namaste": "दुनिया"}
 
 @appn.get("/call/start")
 def start_call():
     return {
         "call id": "CALL 123",
         "status": "starting..."
+    }
+
+@appn.get("/hello/{name}")
+def greet(name: str):
+    return {
+        "greeting": f"Hello {name}",
+        "length_of_name": len(name)
+    }
+
+@appn.get("/info")
+def info(age: int, city: str = "Unknown"):
+    return {
+        "age": age,
+        "city": city,
+        "adult": age >= 18
     }
 
 """
@@ -23,4 +38,8 @@ uvicorn fast:appn --reload --host 0.0.0.0
 
 https://glorious-pancake-9g6v5jvj59vhp69v-8000.app.github.dev
 https://glorious-pancake-9g6v5jvj59vhp69v-8000.app.github.dev/call/start
+https://glorious-pancake-9g6v5jvj59vhp69v-8000.app.github.dev/hello/Ishaan
+https://glorious-pancake-9g6v5jvj59vhp69v-8000.app.github.dev/info?age=19&city=ghaziabad
+
+Ctrl + C to stop the server
 """
